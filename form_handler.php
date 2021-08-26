c42ad43d78862513a1a7dc684a7d8bf717831ad5
anusha-k12 <89342978+anusha-k12@users.noreply.github.com>
2021-08-26 09:48:29 +0530

    Update form_handler.php

--- form_handler.php
+++ form_handler.php
@@ -13,7 +13,7 @@ $email_body = "User Name: $name.\n".
                 "Subject: $subject.\n".
                 "User Message: $message.\n";
 
-$to = 'helloworld9583@gmail.com';
+$to = 'anubangera2000@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
